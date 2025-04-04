408349df6664f61320b38d32b816a4efcba490e6
kristiandiana <kristian8diana@gmail.com>
2025-04-04 01:53:17 -0400

    Added walking skeleton UnitTests.java file

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/UnitTests.java
@@ -0,0 +1,12 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UnitTests {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(1, 1); // Just to check test runs
+    }
+}
